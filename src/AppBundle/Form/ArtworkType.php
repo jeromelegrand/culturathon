38ee75b5a971ca34834ae57390ad6a179c94aba2
Brice Jaugin <syara.legnome@gmail.com>
2018-06-13 17:57:10 +0200

    modif artwork form

--- src/AppBundle/Form/ArtworkType.php
+++ src/AppBundle/Form/ArtworkType.php
@@ -2,6 +2,10 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\Artist;
+use AppBundle\Entity\ArtStyle;
+use AppBundle\Entity\Museum;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,7 +17,27 @@ class ArtworkType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')->add('characteristics')->add('description')->add('artist')->add('museum')->add('artStyle');
+        $builder->add('name')
+            ->add('characteristics')
+            ->add('description')
+            ->add('artist', EntityType::class, [
+                'label' => 'Artist',
+                'class' => Artist::class,
+                'choice_label' => 'firstName',
+                'invalid_message' => 'Le champ est invalide.',
+            ])
+            ->add('museum', EntityType::class, [
+                'label' => 'Musée',
+                'class' => Museum::class,
+                'choice_label' => 'name',
+                'invalid_message' => 'Le champ est invalide.',
+            ])
+            ->add('artStyle', EntityType::class, [
+                'label' => 'Style',
+                'class' => ArtStyle::class,
+                'choice_label' => 'name',
+                'invalid_message' => 'Le champ est invalide.',
+            ]);
     }/**
      * {@inheritdoc}
      */
